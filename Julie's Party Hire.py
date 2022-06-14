a6927a7ab3754cd14a0f308d9dcb9300b2e60f4f
st21110 <st21110@ormiston.school.nz>
2022-06-14 19:27:23 +1200

    Imports

---
+++ Julie's Party Hire.py
@@ -0,0 +1,14 @@
+#Date: 3/6/22
+#Author: Prisha
+
+"""
+Julie's Party Hire. 
+
+This code will use functions along side methods to produce an outcome. 
+
+"""
+
+#imports for the project
+from tkinter import *
+from tkinter import ttk
+
