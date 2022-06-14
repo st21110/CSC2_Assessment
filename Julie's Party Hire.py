bbcc1a6c82a14a7e323a72d48cff3dbf16a2f400
st21110 <st21110@ormiston.school.nz>
2022-06-14 19:32:06 +1200

    Roots, Quit Function and Entry Labels

--- Julie's Party Hire.py
+++ Julie's Party Hire.py
@@ -12,3 +12,16 @@ This code will use functions along side methods to produce an outcome.
 from tkinter import *
 from tkinter import ttk
 
+#Defining and Naming the root
+root = Tk()
+root.title("Julie's Party Hires")
+
+def quit():
+    exit()
+
+#Labels for entries
+Label(root, text="Customer Name:").grid(row=4,column=2)
+Label(root, text="Item Hired:").grid(row=5,column=2)
+Label(root, text="Quantity Hired:").grid(row=6,column=2)
+Label(root, text="Receipt Number:").grid(row=7,column=2)
+Label(root, text="Row Number:").grid(row=11,column=2)
