a0e4c711c274b89b5e2e80eff9d0728f48504e35
st21110 <st21110@ormiston.school.nz>
2022-06-21 10:20:47 +1200

    Entry check function + new comments

--- Julie's Party Hire.py
+++ Julie's Party Hire.py
@@ -10,12 +10,14 @@ This code will use functions along side methods to produce an outcome.
 
 #imports for the project
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk #for nicer boxes and buttons
+from tkinter import messagebox #for error messages 
 
 #Defining and Naming the root
 root = Tk()
 root.title("Julie's Party Hires")
 
+#Quit Function
 def quit():
     exit()
 
@@ -26,6 +28,9 @@ Label(root, text="Quantity Hired:").grid(row=6,column=2)
 Label(root, text="Receipt Number:").grid(row=7,column=2)
 Label(root, text="Row Number:").grid(row=11,column=2)
 
+#Main title heading
+Label(root,text="Julie's Party Hire", font='bold', fg='#f00').grid(row=1,column=3)
+
 #List values for item hire (updatable list)
 item_names = ["Chairs","Tables","Speakers","Coloured Lights",
 "Helium Tanks", "Gas Barbeque","Marquees","Inflatable Pool",
@@ -33,7 +38,6 @@ item_names = ["Chairs","Tables","Speakers","Coloured Lights",
 "Bouncy Castle","Cake Stand" ,"Fog Machine"]
 
 #Entries
-
 #Customer Name
 customer_name = Entry(root)
 customer_name.grid(row=4,column=3) #Placement
@@ -61,30 +65,31 @@ def entrybox_text():
     customer_name.insert(0, "Full Name") #text inside entry box
     def temp_text(e): #function to delete text in entry box
         (customer_name).delete(0,END)
-    customer_name.bind("<FocusIn>", temp_text) #Deletes the temporory text once clicked on
+    customer_name.bind("<FocusIn>", temp_text) #deletes the temporory text once clicked on
 
     quantity_hired.insert(0, "Item No. (1-500)") #text inside entry box
     def temp_text2(e): #function to delete text in entry box
         (quantity_hired).delete(0,END)
-    quantity_hired.bind("<FocusIn>", temp_text2) #Deletes the temporory text once clicked on
+    quantity_hired.bind("<FocusIn>", temp_text2) #deletes the temporory text once clicked on
 
     receipt_number.insert(0, "Receipt No.") #text inside entry box
     def temp_text3(e): #function to delete text in entry box
         (receipt_number).delete(0,END)
-    receipt_number.bind("<FocusIn>", temp_text3) #Deletes the temporory text once clicked on
+    receipt_number.bind("<FocusIn>", temp_text3) #deletes the temporory text once clicked on
 
 def deleterow_text():
     delete_item.insert(0, "Row No.") #text inside entry box
     def temp_text4(e): #function to delete text in entry box
         (delete_item).delete(0,END)
-    delete_item.bind("<FocusIn>", temp_text4) #Deletes the temporory text once clicked on
+    delete_item.bind("<FocusIn>", temp_text4) #deletes the temporory text once clicked on
 
 #Print details function 
 def print_details():
-    global total_entries, name_count
+    global total_entries, name_count #global variables used
     name_count = 0
     
     while name_count < total_entries :
+        #Labels that print the details
         Label(root, text=name_count).grid(column=0,row=name_count+13) 
         Label(root, text=(customer_details[name_count][0])).grid(column=1,row=name_count+13)
         Label(root, text=(customer_details[name_count][1])).grid(column=2,row=name_count+13)
@@ -93,7 +98,7 @@ def print_details():
         name_count +=  1
 
 def append_details():
-    global customer_details,customer_name,item_hired,quantity_hired,receipt_number,total_entries
+    global customer_details,customer_name,item_hired,quantity_hired,receipt_number,total_entries #global variables used
     if len(customer_name.get()) != 0:
         customer_details.append([customer_name.get(),item_hired.get(),quantity_hired.get(),receipt_number.get()])
         customer_name.delete(0,END)
@@ -104,7 +109,7 @@ def append_details():
         entrybox_text()
 
 def delete_row ():
-    global customer_details, delete_item, total_entries, name_count
+    global customer_details, delete_item, total_entries, name_count #global variables used
     del customer_details[int(delete_item.get())]
     total_entries = total_entries -1
     delete_item.delete(0,END)
@@ -116,8 +121,77 @@ def delete_row ():
     print_details()
     deleterow_text()
 
+#Checking for input errors in 
+
+def entry_check():
+    global customer_name, item_hired, quantity_hired, receipt_number, total_entries #global variables used
+    input_check = 0 #variable for input check
+
+    #if customer name is empty
+    if len(customer_name.get()) == 0:
+        messagebox.showerror("Customer name", "Customer name is required") #message box with error will pop up
+        input_check = 1 #input check will equal 1 
+
+    #if customer name is same as entrybox text
+    if customer_name.get() == "Full Name":
+        messagebox.showerror("Customer name", "Customer name is required") #message box with error will pop up
+        input_check = 1 #input check will equal 1 
+    
+    #if item hired is empty
+    if len(item_hired.get()) == 0:
+        messagebox.showerror("Item Hired", "Item name is required") #message box with error will pop up
+        input_check = 1 #input check will equal 1 
+
+    try: 
+        #if receipt number is same as entrybox text
+        if receipt_number.get() == "Receipt No.":
+            messagebox.showerror("Receipt Number", "Receipt Number is required") #message box with error will pop up
+            input_check = 1 #input check will equal 1 
+    
+        #if receipt number is empty
+        if len(receipt_number.get()) == 0:
+            messagebox.showerror("Receipt Number", "Receipt Number is required") #message box with error will pop up
+            input_check = 1 #input check will equal 1 
+    
+        #if receipt number doesnt equal to a number 0>
+        if (receipt_number.get().isdigit):
+            if int(receipt_number.get()) == 0:
+                messagebox.showerror("Receipt Number", "Receipt Number must be a number") #message box with error will pop up
+                input_check = 1 #input check will equal 1    
+
+    except:#in all other cases (strings or invalid characters)
+            messagebox.showerror("Receipt Number", "Receipt Number must be a number") #message box with error will pop up
+            input_check = 1 #input check will equal 1      
+    
+    try:
+        #if quantity hired is the same as entrybox text
+        if quantity_hired.get() == "Item No. (1-500)":
+            messagebox.showerror("Quantity Hired", "Item quantity is required") #message box with error will pop up
+            input_check = 1 #input check will equal 1 
+
+        #if quanitiy hired is empty
+        if len(quantity_hired.get()) == 0:
+            messagebox.showerror("Quantity Hired", "Item quantity is required") #message box with error will pop up
+            input_check = 1 #input check will equal 1 
+
+        #if quantity hired is 0 or more than 500
+        if (quantity_hired.get().isdigit):
+            if int(quantity_hired.get()) == 0 or int(quantity_hired.get()) > 500:
+                messagebox.showerror("Quantity Hired", "Quantity hired must be between 1 and 500") #message box with error will pop up
+                input_check = 1 #input check will equal 1 
+    
+    except:#in all other cases (strings or invalid characters)
+            messagebox.showerror("Quantity Hired", "Please enter a number") #message box with error will pop up
+            input_check = 1 #input check will equal 1 
+    
+    #if input check equals 0 there is no errors
+    if input_check == 0 : 
+        append_details() #details will be appended
+
+
+
 #Buttons
-Button(root, text="Append",command=append_details,width=17).grid(row=8,column=3)
+Button(root, text="Append",command=entry_check,width=17).grid(row=8,column=3)
 Button(root, text="Print Details",command=print_details,width=17).grid(row=8,column=4)
 Button(root,text="Quit",command=quit).grid(row=13,column=5)
 Button(root,text="Delete",command=delete_row).grid(row=11,column=4)
