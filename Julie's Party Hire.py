0489f30a13153910202549625679e424b3a94815
st21110 <st21110@ormiston.school.nz>
2022-06-20 20:22:18 +1200

    Added Entries

--- Julie's Party Hire.py
+++ Julie's Party Hire.py
@@ -25,3 +25,30 @@ Label(root, text="Item Hired:").grid(row=5,column=2)
 Label(root, text="Quantity Hired:").grid(row=6,column=2)
 Label(root, text="Receipt Number:").grid(row=7,column=2)
 Label(root, text="Row Number:").grid(row=11,column=2)
+
+#List values for item hire (updatable list)
+item_names = ["Chairs","Tables","Speakers"]
+
+#Entries
+
+#Customer Name
+customer_name = Entry(root)
+customer_name.grid(row=4,column=3) #Placement
+
+#Item Hired
+items=StringVar()
+item_hired = ttk.Combobox(root, textvariable = items, values =(item_names),
+state = "readonly",width=17)
+item_hired.grid(row=5, column=3)
+
+#Quantity Hired
+quantity_hired = Entry(root)
+quantity_hired.grid(row=6,column=3)
+
+#Receipt Number
+receipt_number = Entry(root)
+receipt_number.grid(row=7,column=3)
+
+#Delete Row
+delete_item = Entry(root)
+delete_item .grid(row=11,column=3)
