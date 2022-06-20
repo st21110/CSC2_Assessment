83a8abd0bafe945bc912be69bc2d3057ff960522
st21110 <st21110@ormiston.school.nz>
2022-06-20 20:38:49 +1200

    Print, Append, Quit Buttons

--- Julie's Party Hire.py
+++ Julie's Party Hire.py
@@ -56,3 +56,40 @@ receipt_number.grid(row=7,column=3)
 delete_item = Entry(root)
 delete_item .grid(row=11,column=3)
 
+#Print details function 
+def print_details():
+    global total_entries, name_count
+    name_count = 0
+    
+    while name_count < total_entries :
+        Label(root, text=name_count).grid(column=0,row=name_count+13) 
+        Label(root, text=(customer_details[name_count][0])).grid(column=1,row=name_count+13)
+        Label(root, text=(customer_details[name_count][1])).grid(column=2,row=name_count+13)
+        Label(root, text=(customer_details[name_count][2])).grid(column=3,row=name_count+13)
+        Label(root, text=(customer_details[name_count][3])).grid(column=4,row=name_count+13)
+        name_count +=  1
+
+def append_details():
+    global customer_details,customer_name,item_hired,quantity_hired,receipt_number,total_entries
+    if len(customer_name.get()) != 0:
+        customer_details.append([customer_name.get(),item_hired.get(),quantity_hired.get(),receipt_number.get()])
+        customer_name.delete(0,END)
+        item_hired.set("")
+        quantity_hired.delete(0,END)
+        receipt_number.delete(0,END)
+        total_entries += 1
+
+#Buttons
+Button(root, text="Append",command=append_details,width=17).grid(row=8,column=3)
+Button(root, text="Print Details",command=print_details,width=17).grid(row=8,column=4)
+Button(root,text="Quit",command=quit).grid(row=13,column=5)
+
+
+def main():
+    global root, customer_details,total_entries
+    customer_details = []
+    total_entries = 0
+    root.mainloop()
+
+
+main() 
